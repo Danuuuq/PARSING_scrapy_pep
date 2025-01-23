6cf8458c7c608ccf24709afd99d802c200d27c74
Daniil Tyunkov <daniil@tyunkov.ru>
2025-01-23 19:46:55 +0300

    Send on 3rd review

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -6,10 +6,16 @@ from datetime import datetime as dt
 
 class PepParsePipeline:
 
+    def __init__(self, output_dir):
+        self.output_dir = output_dir
+        os.makedirs(output_dir, exist_ok=True)
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        feeds = crawler.settings.get('FEEDS')
+        return cls(os.path.dirname(next(iter(feeds))))
+
     def open_spider(self, spider):
-        feeds = spider.settings.get('FEEDS')
-        self.output_dir = os.path.dirname(next(iter(feeds)))
-        os.makedirs(self.output_dir, exist_ok=True)
         self.result = defaultdict(int)
 
     def process_item(self, item, spider):
@@ -19,18 +25,11 @@ class PepParsePipeline:
     def close_spider(self, spider):
         date = dt.strftime(dt.now(), '%Y-%m-%d_%H-%M-%S')
         filename = os.path.join(self.output_dir, f'status_summary_{date}.csv')
-        # self.result['Total'] = sum(self.result.values())
         with open(filename, mode='w', encoding='UTF-8', newline='') as csvfile:
-            breakpoint()
-            # fields = ['Статус', 'Количество']
-            # rows = [{fields[0]: status, fields[1]: count}
-            #         for status, count in self.result.items()]
-            # writer = csv.DictWriter(csvfile, fieldnames=fields)
             rows = (
                 ('Статус', 'Количество'),
                 *self.result.items(),
                 ('Total', sum(self.result.values()))
             )
-            writer = csv.DictWriter(csvfile)
-            # writer.writeheader()
+            writer = csv.writer(csvfile)
             writer.writerows(rows)
